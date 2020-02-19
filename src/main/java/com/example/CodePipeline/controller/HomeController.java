0723e8e669d9b48fec1436ad466fc07df574b71b
Piyush Verma <piyushverma95@users.noreply.github.com>
2020-02-19 12:18:28 +0530

    System.currentTimeMillis()

--- src/main/java/com/example/CodePipeline/controller/HomeController.java
+++ src/main/java/com/example/CodePipeline/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String hello() {
-		return "Good Afternoonmmmmm!";
+		return "Good Afternoonmmmmm!   :  "+System.currentTimeMillis();
 	}
 	
 }
